6fe8162c690b343ac5471e67d6a34e668ada438c
Cicorschi Alex <cicorschialex@gmail.com>
2023-06-04 01:37:20 +0200

    added timer and hud element to display it

--- Assets/kaboomcombat/Code/Scripts/MainGame/HudController.cs
+++ Assets/kaboomcombat/Code/Scripts/MainGame/HudController.cs
@@ -8,10 +8,20 @@ namespace kaboomcombat
     public class HudController : MonoBehaviour
     {
         private SessionManager sessionManager;
+        public TextMeshProUGUI textTimer;
 
         void Start()
         {
             sessionManager = FindObjectOfType<SessionManager>();
+        }
+
+
+        public void UpdateTimer(float time)
+        {
+            int minutes = Mathf.FloorToInt(time / 60);
+            int seconds = Mathf.FloorToInt(time % 60);
+
+            textTimer.text = string.Format("{0:0}:{1:00}", minutes, seconds);
         }
     }
 }
--- Assets/kaboomcombat/Code/Scripts/MainGame/SessionManager.cs
+++ Assets/kaboomcombat/Code/Scripts/MainGame/SessionManager.cs
@@ -10,13 +10,45 @@ namespace kaboomcombat
 {
     public class SessionManager : MonoBehaviour
     {
+        public float time = 180;
+
         public List<GameObject> objectList;
+        public HudController hudController;
 
 
         void Start()
         {
             // Create the objectList
             objectList = new List<GameObject>(); 
+            hudController = FindObjectOfType<HudController>();
+
+            time++;
+        }
+
+
+        private void FixedUpdate()
+        {
+            if(Mathf.FloorToInt(time) > 0)
+            {
+                UpdateTime();
+            }
+            else{
+                GameOver(true);
+            }
+        }
+
+
+        private void GameOver(bool timeOut)
+        {
+
+        }
+
+
+        private void UpdateTime()
+        {
+            time -= Time.deltaTime;
+
+            hudController.UpdateTimer(time);
         }
     }
 }
